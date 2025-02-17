654a74601bcd5edbfb08cd0428222d5eda03a189
AmmarNadeem <ammarpk2004@gmail.com>
2025-02-17 16:50:57 +0500

    Configure MongoDB database with Mongoose ORM

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 const app = express();
+const connStr: string = process.env.CONNECTION_STRING!;
+mongoose
+  .connect(connStr)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch(() => {
+    console.log("Database Connection Error");
+  });
 app.listen(3000, () => {
   console.log("first");
 });
